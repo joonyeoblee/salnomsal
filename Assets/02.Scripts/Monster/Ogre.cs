c46c381a7e694df8e46a038f9b7b2f30c356ac26
joonyeoblee <101649294+joonyeoblee@users.noreply.github.com>
2025-04-03 19:55:42 +0900

    몬스터 스킬코드 베이스에 병합

--- Assets/02.Scripts/Monster/Ogre.cs
+++ Assets/02.Scripts/Monster/Ogre.cs
@@ -1,23 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
-using Jun.Skill;
-using UnityEngine;
 
 namespace Jun.Monster
 {
     public class Ogre : MonsterBase
     {
-        public bool IsMyTurn;
-        MonsterSkill _skillComponent;
-
-        void Start()
+        protected override void Start()
         {
-            _skillComponent = GetComponent<MonsterSkill>();
-
+            base.Start();
+            
             _health = MaxHealth;
             _mana = MaxMana;
 
-            List<Func<EnemyCharacter, int>> conditionalList = new List<Func<EnemyCharacter, int>>
+            List<Func<Character, int>> conditionalList = new()
             {
                 target => target.CurrentHealth < target.MaxHealth * 0.3f ? 5 : 0, // Skill1
                 target => target.IsDefending ? 10 : 0 // Skill2
@@ -26,45 +21,11 @@ namespace Jun.Monster
             _skillComponent.SetConditionalPriorities(conditionalList);
         }
 
-        void Update()
-        {
-            if (IsMyTurn)
-            {
-                IsMyTurn = false;
-
-                if (_skillComponent == null || _target == null)
-                {
-                    Debug.LogWarning("스킬 또는 타겟이 없음, 기본 공격");
-                    Attack();
-                    return;
-                }
-
-                SkillDecision decision = _skillComponent.ChooseSkillWithIndex(_target);
-                Debug.Log(decision.Skill == null);
-                if (decision == null || decision.Skill == null)
-                {
-                    Attack();
-                    return;
-                }
-
-                switch (decision.Index)
-                {
-                case 0:
-                    Skill1();
-                    break;
-                case 1:
-                    Skill2();
-                    break;
-                default:
-                    Attack();
-                    break;
-                }
-            }
-        }
-
         protected override void Attack()
         {
             base.Attack();
+
+            if (_target == null) return;
 
             Damage damage = new Damage(DamageType.Melee, AttackPower, gameObject);
             _target.TakeDamage(damage);
@@ -74,7 +35,10 @@ namespace Jun.Monster
         {
             base.Skill1();
 
-            Damage damage = new Damage(DamageType.Melee, AttackPower * _skillComponent.skillDataList[0].SkillMultiplier, gameObject); // 예시 값
+            if (_target == null) return;
+
+            float damageAmount = AttackPower * _skillComponent.skillDataList[0].SkillMultiplier;
+            Damage damage = new Damage(DamageType.Melee, damageAmount, gameObject);
             _target.TakeDamage(damage);
         }
 
@@ -82,16 +46,11 @@ namespace Jun.Monster
         {
             base.Skill2();
 
-            Damage damage = new Damage(DamageType.Magic, _skillComponent.skillDataList[1].SkillMultiplier, gameObject); // 예시 값
+            if (_target == null) return;
+
+            float damageAmount = _skillComponent.skillDataList[1].SkillMultiplier;
+            Damage damage = new Damage(DamageType.Magic, damageAmount, gameObject);
             _target.TakeDamage(damage);
-        }
-
-        public override void StartTurn()
-        {
-            base.StartTurn();
-
-            IsMyTurn = true;
-            _target = GetTarget();
         }
     }
 }
