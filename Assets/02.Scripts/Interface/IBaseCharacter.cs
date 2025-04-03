0eb50f297ba0dc375dbc2fbfce4b86bb4e96f631
joonyeoblee <101649294+joonyeoblee@users.noreply.github.com>
2025-04-03 09:42:41 +0900

    폴더 생성

---
+++ Assets/02.Scripts/Interface/IBaseCharacter.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public interface IBaseCharacter
+{
+    void Attack();
+    void Skill1();
+    void Skill2();
+    void OnDamage();
+    void OnDeath();
+}
---
+++ Assets/02.Scripts/MonsterBase.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace Jun.Monster
+{
+    public class MonsterBase : MonoBehaviour, IBaseCharacter
+    {
+        public void Attack()
+        {
+            throw new System.NotImplementedException();
+        }
+        public void Skill1()
+        {
+            throw new System.NotImplementedException();
+        }
+        public void Skill2()
+        {
+            throw new System.NotImplementedException();
+        }
+        public void OnDamage()
+        {
+            throw new System.NotImplementedException();
+        }
+        public void OnDeath()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+}
