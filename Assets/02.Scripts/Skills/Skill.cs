5c61e3824f874c4186a44710953b080659363cfc
Park Jinhyeok <harunohi0808@gmail.com>
2025-04-03 15:50:18 +0900

    Unity version up

---
+++ Assets/02.Scripts/Skills/Skill.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+public class Skill : MonoBehaviour
+{
+    public SkillDataSO SkillData;
+
+
+}
---
+++ Assets/02.Scripts/Skills/SkillDataSO.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public enum SkillType
+{
+    Attack,
+    Buff,
+    Debuff,
+    Heal
+}
+
+public enum SkillRange
+{
+    Single,
+    Global,
+}
+
+[CreateAssetMenu(fileName = "SkillDataSO", menuName = "Scriptable Objects/SkillDataSO")]
+public class SkillDataSO : ScriptableObject
+{
+    public string SkillName;
+    public string SkillDescription;
+    public int SkillCost;
+    public int SkillCooldown;
+    public float SkillMultiplier;
+    public SkillType SkillType;
+    public SkillRange SkillRange;
+}
+
