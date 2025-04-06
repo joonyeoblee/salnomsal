6f8fa5f8d1968d24c78b894e19455bf723186bc4
joonyeoblee <101649294+joonyeoblee@users.noreply.github.com>
2025-04-06 11:34:27 +0900

    시간 느리게 기능 테스트 추가

--- Assets/02.Scripts/MiniGame/Magic.cs
+++ Assets/02.Scripts/MiniGame/Magic.cs
@@ -1,84 +1,57 @@
-using System;
-using System.Collections;
+using Jun.MiniGame;
+using MoreMountains.Tools;
 using UnityEngine;
-using UnityEngine.UI;
 
 namespace Jun.MiniGame
 {
+
     public class Magic : MonoBehaviour
     {
-        public Image FillImage;
+        public MMProgressBar ProgressBar;
         public char assignedKey;
         private bool _isKeyPressed = false;
         private MatchPattern _matchPattern;
 
+        public float MaxTime;
+        float _currentTime = 0f;
+
         void Start()
         {
-            
-            FillImage = GetComponent<Image>();
             _matchPattern = GetComponentInParent<MatchPattern>();
             _matchPattern.RegisterMagic(this);
-            FillImage.fillAmount = 0;
-            StartCoroutine(CheckKeyTimeout());
-            StartCoroutine(FillImageOverTime());
+            ProgressBar = GetComponent<MMProgressBar>();
+            ProgressBar.FillMode = MMProgressBar.FillModes.FillAmount;
+            ProgressBar.InitialFillValue = 0f;
+        }
+
+        private void Update()
+        {
+            _currentTime += Time.deltaTime;
+
+            // MMProgressBar 업데이트
+            if (ProgressBar != null)
+            {
+                ProgressBar.UpdateBar(_currentTime,0,MaxTime);
+            }
+
+            if (_currentTime >= MaxTime)
+            {
+                _isKeyPressed = false;
+                _matchPattern.UnregisterMagic(this);
+                _matchPattern.Fail();
+            }
         }
 
         public void TryResolve()
         {
+            float duration = 1f;
             if (_isKeyPressed) return;
 
             _isKeyPressed = true;
-            _matchPattern.UnregisterMagic(this); 
+            _matchPattern.UnregisterMagic(this);
             _matchPattern.AddCount();
             Debug.Log("성공 매직");
             Destroy(gameObject);
         }
-
-        IEnumerator CheckKeyTimeout()
-        {
-            yield return new WaitForSeconds(1f);
-
-            if (!_isKeyPressed)
-            {
-                _matchPattern.Fail();
-                _matchPattern.UnregisterMagic(this);
-                Destroy(gameObject);
-            }
-        }
-
-        IEnumerator FillImageOverTime()
-        {
-            float elapsed = 0f;
-            float duration = 1f;
-
-            while (elapsed < duration)
-            {
-                if (_isKeyPressed) yield break;
-
-                elapsed += Time.deltaTime;
-                float fill = Mathf.Clamp01(elapsed / duration);
-
-                if (FillImage != null)
-                    FillImage.fillAmount = fill;
-
-                yield return null;
-            }
-
-            // 시간 초과되면 실패 처리 (중복 방지용 안전 장치)
-            if (!_isKeyPressed)
-            {
-                _matchPattern.Fail();
-                _matchPattern.UnregisterMagic(this);
-                Destroy(gameObject);
-            }
-        }
     }
-
-    // void OnDestroy()
-    // {
-    //     if (_matchPattern != null)
-    //     {
-    //         _matchPattern.UnregisterMagic(this);
-    //     }
-    // }
 }
