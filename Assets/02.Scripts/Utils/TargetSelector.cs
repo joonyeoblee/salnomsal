7fb9b8a244a94080deb89e83bcab26f0cd9256ca
joonyeoblee <101649294+joonyeoblee@users.noreply.github.com>
2025-04-07 13:07:17 +0900

    아웃라인 에셋 설정

--- Assets/02.Scripts/Utils/TargetSelector.cs
+++ Assets/02.Scripts/Utils/TargetSelector.cs
@@ -1,14 +1,27 @@
 ﻿using UnityEngine;
 using UnityEngine.EventSystems;
 [RequireComponent(typeof(Transform))]
-public class TargetSelector : MonoBehaviour, IPointerClickHandler
+public class TargetSelector : MonoBehaviour, IPointerClickHandler, IPointerEnterHandler, IPointerExitHandler
 {
+    
     public void OnPointerClick(PointerEventData eventData)
     {
         Debug.Log("clicked");
         CombatManager.Instance.SetTarget(GetComponent<ITargetable>());
     }
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        Debug.Log("Mouse Enter");
 
+        // 예: 하이라이트 처리
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        Debug.Log("Mouse Exit");
+
+        // 예: 하이라이트 제거
+    }
     void Reset()
     {
         if (GetComponent<Collider>() == null)
