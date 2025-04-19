ce856e6f3ce29083f07a9665f9042e2128e3940c
SeongIl037 <kerect@naver.com>
2025-04-19 11:43:01 +0900

    Avoid 수정

--- Assets/02.Scripts/MiniGame/AvoidGame/AvoidSpawner.cs
+++ Assets/02.Scripts/MiniGame/AvoidGame/AvoidSpawner.cs
@@ -19,23 +19,17 @@ public class AvoidSpawner : MonoBehaviour
         
     }
     
-    //난이도 세팅
-    public void DifficultySet(Avoid avoid)
-    {
-        MaxTime = avoid.MaxTime;
-        MinTime = avoid.MinTime;
-    }
     // 위치 정하기
     private IEnumerator SpawnPositionSet()
     {   
         for (int i = 0; i < SpawnCount; i++)
         {
             float randomSpawn = UnityEngine.Random.Range(MinTime, MaxTime);
-            
             int randomPosition = UnityEngine.Random.Range(0, 18);
 
             float angle = randomPosition * 20f;
             float degAngle = angle *  Mathf.Deg2Rad;
+            
             Vector3 direction = new Vector3(Mathf.Cos(degAngle), Mathf.Sin(degAngle), 0f);
             Vector3 spawn = SpawnPos + direction * Distance;
             
@@ -52,9 +46,11 @@ public class AvoidSpawner : MonoBehaviour
     //게임 시작과 동시에 카운트를 받아
     public void SpawnStart( int count, Avoid avoid)
     {
-        DifficultySet(avoid);
         SpawnCount = count;
+        MaxTime = avoid.MaxTime;
+        MinTime = avoid.MinTime;
         StartCoroutine(SpawnPositionSet());
+
     }
 
 }
