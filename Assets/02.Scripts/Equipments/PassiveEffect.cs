9503446793eccb80da3df80b0010b8f250b8637d
joonyeoblee <101649294+joonyeoblee@users.noreply.github.com>
2025-05-17 18:00:54 +0900

    장비 구조 파악 및 정리

--- Assets/02.Scripts/Equipments/PassiveEffect.cs
+++ Assets/02.Scripts/Equipments/PassiveEffect.cs
@@ -1,7 +1,28 @@
 using System;
+using Random = UnityEngine.Random;
 
 namespace Equipment
 {
+    public enum Rarity
+    {
+        Common,
+        Rare,
+        Epic,
+        Legendary
+    }
+
+
+    public class FloatRange
+    {
+        public float Min;
+        public float Max;
+
+        public float GetRandom()
+        {
+            return Random.Range(Min, Max);
+        }
+    }
+
     [Serializable]
     public class PassiveEffect
     {
