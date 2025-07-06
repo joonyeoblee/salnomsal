29bf6cd138cbc646c413a08e6c3ca04fe068e5b1
SeongIl037 <kerect@naver.com>
2025-07-06 23:43:13 +0900

    미니게임 포스트 프로세싱 조명 완료, 원거리 미니게임 화살 스프라이트 적용

--- Assets/02.Scripts/MiniGame/AvoidGame/BulletMove.cs
+++ Assets/02.Scripts/MiniGame/AvoidGame/BulletMove.cs
@@ -14,13 +14,19 @@ namespace SeongIl
         void Start()
         {
             MyPlayer = GameObject.FindGameObjectWithTag("Player");
+
             _direction = MyPlayer.transform.position - transform.position;
             _direction.Normalize();
-            
+
+            float radian = Mathf.Atan2(_direction.y, _direction.x);
+            float angle = Mathf.Rad2Deg * radian - 135f ;
+
+            // 회전 적용 (Z축 기준, 2D 전용)
+            transform.eulerAngles = new Vector3(0, 0, angle);
         }
         void Update()
         {
-            transform.Translate(_direction * Speed * Time.deltaTime);
+            transform.position += (Vector3)_direction * Speed * Time.deltaTime;
         }
         
     }
