9b40a53bbd5fa2fe113d93ccee33e151120c40b4
SeongIl037 <kerect@naver.com>
2025-07-07 21:50:10 +0900

    DDD 하려는데 막힘

---
+++ Assets/02.Scripts/Slot/4.UI/UI_Inventory.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_Inventory : MonoBehaviour
+{
+    // 현재 인벤토리는 무엇인가
+    public EInventoryType InventoryType;
+    public List<UI_Slot> SlotList;
+    
+    
+    
+}
